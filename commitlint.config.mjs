37777a8a14a9a5d6de07cfbc0058d83363a1d9d3
Josh <lekgold@gmail.com>
2026-08-06 20:48:59 -0400

    chore(commitlint): allow data commit type

--- commitlint.config.mjs
+++ commitlint.config.mjs
@@ -1,3 +1,23 @@
 export default {
   extends: ['@commitlint/config-conventional'],
+  rules: {
+    'type-enum': [
+      2,
+      'always',
+      [
+        'build',
+        'chore',
+        'ci',
+        'data',
+        'docs',
+        'feat',
+        'fix',
+        'perf',
+        'refactor',
+        'revert',
+        'style',
+        'test',
+      ],
+    ],
+  },
 };
